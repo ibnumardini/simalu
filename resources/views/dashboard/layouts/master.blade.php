47a5ea4cb5547aead674edc9c361dd4346046ac2
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2024-04-26 18:30:24 +0700

    feat(dashboard): setup minimalist tabler template admin

---
+++ resources/views/dashboard/layouts/master.blade.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<!--
+* Tabler - Premium and Open Source dashboard template with responsive and high quality UI.
+* @version 1.0.0-beta20
+* @link https://tabler.io
+* Copyright 2018-2023 The Tabler Authors
+* Copyright 2018-2023 codecalm.net Paweł Kuna
+* Licensed under MIT (https://github.com/tabler/tabler/blob/master/LICENSE)
+-->
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>@yield('title') - SIMALU - Sistem Informasi Manajemen Alumni.</title>
+    <!-- CSS files -->
+    <link href=" {{ asset("/css/tabler.min.css") }}" rel="stylesheet" />
+    <style>
+        @import url('https://rsms.me/inter/inter.css');
+
+        :root {
+            --tblr-font-sans-serif: 'Inter Var', -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif;
+        }
+
+        body {
+            font-feature-settings: "cv03", "cv04", "cv11";
+        }
+    </style>
+</head>
+
+<body class="layout-fluid">
+    @yield('content')
+
+    <!-- Tabler Core -->
+    <script src="{{ asset("/js/tabler.min.js") }}" defer></script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect("/", "/dashboard");
+
+Route::get("/dashboard", [DashboardController::class, "index"]);
