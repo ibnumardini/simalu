0bd4589b001452ac8fe6f65ab55986b05cf4db28
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2024-04-28 23:06:56 +0700

    feat(dashboard): celan unused components

---
+++ resources/views/dashboard/partials/footer.blade.php
@@ -0,0 +1,31 @@
+<footer class="footer footer-transparent d-print-none">
+    <div class="container-xl">
+        <div class="row text-center align-items-center flex-row-reverse">
+            <div class="col-lg-auto ms-lg-auto">
+                <ul class="list-inline list-inline-dots mb-0">
+                    <li class="list-inline-item">v0.1.0-{{ rand() }}</li>
+                </ul>
+            </div>
+            <div class="col-12 col-lg-auto mt-3 mt-lg-0">
+                <ul class="list-inline list-inline-dots mb-0">
+                    <li class="list-inline-item">
+                        Copyright &copy; {{ date('Y') }}
+                        <a href="https://tabler.io/" class="link-secondary">Tabler</a>.
+                        All rights reserved.
+                    </li>
+                    <li class="list-inline-item">
+                        App developed w/
+                        <!-- Download SVG icon from http://tabler-icons.io/i/heart -->
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon text-pink icon-filled icon-inline"
+                            width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                            fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
+                        </svg>
+                        by <a class="link-secondary" href="https://fatkur.id">Ibnu Mardini</a>.
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
