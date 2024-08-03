76708becf60bc6335a4ae99351b695d40258e894
wardanaricho <wardanaricho0@gmail.com>
2024-08-03 21:00:59 +0700

    feat: add alumni controller method index

--- app/Http/Controllers/AlumniController.php
+++ app/Http/Controllers/AlumniController.php
@@ -4,15 +4,38 @@ namespace App\Http\Controllers;
 
 use App\Models\Alumni;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class AlumniController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request): View
     {
-        //
+        $paginate = 10;
+
+        $query = Alumni::with(['user', 'school']);
+        $searchQuery = $request->q;
+
+        if ($request->has('q')) {
+            $query->where(function ($q) use ($searchQuery) {
+                $q->where('mobile', 'like', "%{$searchQuery}%")
+                    ->orWhere('address', 'like', "%{$searchQuery}%")
+                    ->orWhereDate('dob', 'like', "%{$searchQuery}%")
+                    ->orWhereDate('registration_at', 'like', "%{$searchQuery}%")
+                    ->orWhereDate('graduation_at', 'like', "%{$searchQuery}%");
+            })->orWhereHas('user', function ($q) use ($searchQuery) {
+                $q->where('first_name', 'like', "%{$searchQuery}%")
+                    ->orWhere('last_name', 'like', "%{$searchQuery}%");
+            })->orWhereHas('school', function ($q) use ($searchQuery) {
+                $q->where('name', 'like', "%{$searchQuery}%");
+            });
+        }
+
+        $alumnis = $query->paginate($perPage = $paginate);
+
+        return view('dashboard.pages.alumnis.index', compact(['alumnis', 'searchQuery']));
     }
 
     /**
