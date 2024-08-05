634e4cadd1071114568167a265d1b32d024906c9
wardanaricho <wardanaricho0@gmail.com>
2024-08-05 19:32:25 +0700

    feat: add new route to get user with json response (user, school)

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AlumniController;
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\SchoolController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::redirect("/", "/dashboard");
@@ -12,6 +13,10 @@ Route::middleware('auth')->group(function () {
     Route::get("/dashboard", [DashboardController::class, "index"])->name('dashboard');
     Route::group(['prefix' => 'master-data'], function () {
         Route::resource("schools", SchoolController::class)->except("show");
+        Route::get('/get-schools', [SchoolController::class, 'getSchool'])->name('get.school');
+
+        Route::get('/get-users', [UserController::class, 'getUser'])->name('get.user');
+
         Route::resource("companies", CompanyController::class);
     });
 
