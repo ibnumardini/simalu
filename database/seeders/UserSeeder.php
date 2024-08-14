ed04542dab639394fec1550159310de5e1196a6c
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2024-08-14 08:31:03 +0700

    refactor(seeder): simplify user creation with each default role

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Constants\RBAC;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -14,13 +15,34 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create([
-            'first_name' => 'Admin',
-            'last_name' => 'Simalu',
-            'email' => 'admin@mail.com',
-            'email_verified_at' => now(),
-            'password' => Hash::make('admin123'),
-            'remember_token' => Str::random(10),
-        ]);
+        /**
+         * User generator with default role.
+         *
+         * email: user@email.com
+         * pass: user123
+         * role: user
+         *
+         * email: admin@email.com
+         * pass: admin123
+         * role: admin
+         *
+         * email: superadmin@email.com
+         * pass: superadmin123
+         * role: superadmin
+         */
+        collect([
+            RBAC::ROLE_USER,
+            RBAC::ROLE_ADMIN,
+            RBAC::ROLE_SUPERADMIN,
+        ])->each(function ($role) {
+            $user = User::create([
+                'first_name' => Str::title($role),
+                'last_name' => 'Simalu',
+                'email' => sprintf('%s@email.com', $role),
+                'password' => Hash::make(sprintf('%s123', $role)),
+            ]);
+
+            $user->assignRole($role);
+        });
     }
 }
