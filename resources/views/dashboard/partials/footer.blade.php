bbe06fb59bf07dd5c06192c522f00df13e3813c9
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2025-09-05 18:08:31 +0700

    fix(footer): update link styles for contributor credits in footer

--- resources/views/dashboard/partials/footer.blade.php
+++ resources/views/dashboard/partials/footer.blade.php
@@ -22,8 +22,8 @@
               <path stroke="none" d="M0 0h24v24H0z" fill="none" />
               <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
             </svg>
-            by <a class="link-secondary" href="https://fatkur.id">Ibnu Mardini</a> & all the coolest <a
-              class="link-secondary" href="https://github.com/ibnumardini/simalu/graphs/contributors"
+            by <a class="link" href="https://fatkur.id">Ibnu Mardini</a> & all the coolest <a
+              class="link" href="https://github.com/ibnumardini/simalu/graphs/contributors"
               target="_blank">contributors</a>.
           </li>
         </ul>
