1349e8ca476e35e307bf9ef9ce62fae832bb53d7
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2025-09-06 16:25:08 +0700

    fix: remove unnecessary app description from the page title

--- resources/views/dashboard/layouts/master.blade.php
+++ resources/views/dashboard/layouts/master.blade.php
@@ -13,7 +13,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>@yield('title') - {{ str()->upper(config('app.name')) }} - {{ config('app.desc') }}.</title>
+    <title>@yield('title') - {{ str()->upper(config('app.name')) }}</title>
     <link rel="shortcut icon" href="{{ asset('/img/favicon.ico') }}" type="image/x-icon">
     <!-- CSS files -->
     <link href=" {{ asset('/css/tabler.min.css') }}" rel="stylesheet" />
