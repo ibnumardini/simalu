27ca8ffef74af49620ab44d24b4c6a54c837c927
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2025-09-07 10:23:42 +0700

    feat(navbar): enhance language switcher with locale display and disable current locale option

--- resources/views/dashboard/partials/navbar.blade.php
+++ resources/views/dashboard/partials/navbar.blade.php
@@ -14,10 +14,15 @@
             <div class="nav-item dropdown me-3">
                 <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
                     <span class="fi fi-{{ app()->getLocale() == $locale_id_ID ? 'id' : 'us' }} me-2"></span>
+                    ({{ app()->getLocale() }})
                 </a>
                 <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                    <a href="{{ route('locale.switch', ['locale' => $locale_id_ID]) }}" class="dropdown-item"><span class="fi fi-id me-2"></span> @lang('messages.navbar.lang.indonesia')</a>
-                    <a href="{{ route('locale.switch', ['locale' => $locale_en_US]) }}" class="dropdown-item"><span class="fi fi-us me-2"></span> @lang('messages.navbar.lang.english')</a>
+                    <a href="{{ route('locale.switch', ['locale' => $locale_id_ID]) }}"
+                        class="dropdown-item {{ app()->getLocale() == $locale_id_ID ? 'disabled' : '' }}"><span
+                            class="fi fi-id me-2"></span> @lang('messages.navbar.lang.indonesia') ({{ $locale_id_ID }})</a>
+                    <a href="{{ route('locale.switch', ['locale' => $locale_en_US]) }}"
+                        class="dropdown-item {{ app()->getLocale() == $locale_en_US ? 'disabled' : '' }}"><span
+                            class="fi fi-us me-2"></span> @lang('messages.navbar.lang.english') ({{ $locale_en_US }})</a>
                 </div>
             </div>
             <div class="nav-item dropdown">
@@ -124,7 +129,15 @@
                             data-bs-auto-close="outside" role="button" aria-expanded="false">
                             <span
                                 class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/package -->
-                                <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-settings"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z" /><path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" /></svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                    stroke-linecap="round" stroke-linejoin="round"
+                                    class="icon icon-tabler icons-tabler-outline icon-tabler-settings">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                    <path
+                                        d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z" />
+                                    <path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
+                                </svg>
                             </span>
                             <span class="nav-link-title">
                                 @lang('messages.navbar.settings')
