b178ee604fbd9861a01008057b408911230075c3
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2025-09-14 22:02:23 +0700

    refactor(fortify): remove two-factor authentication feature and related migration

--- config/fortify.php
+++ config/fortify.php
@@ -149,11 +149,11 @@ return [
         // Features::emailVerification(),
         Features::updateProfileInformation(),
         Features::updatePasswords(),
-        Features::twoFactorAuthentication([
-            'confirm' => true,
-            'confirmPassword' => false,
-            // 'window' => 0,
-        ]),
+        // Features::twoFactorAuthentication([
+        //     'confirm' => true,
+        //     'confirmPassword' => false,
+        //     // 'window' => 0,
+        // ]),
     ],
 
 ];
